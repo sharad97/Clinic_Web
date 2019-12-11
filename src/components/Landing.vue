b688ae5bfb813c2d9e8e9c6fc89a8c932254c6e6
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-11 02:33:32 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -67,22 +67,11 @@ body {
 <div class="row">
   <div class="column">
     <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_1.jpg" style="width:100%">
-  </div>
-
-  <div class="column">
-  <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_2.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_3.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_4.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_5.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_6.jpg" style="width:100%">
+    <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_2.jpg" style="width:100%">
+    <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_3.jpg" style="width:100%">
+    <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_4.jpg" style="width:100%">
+    <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_5.jpg" style="width:100%">
+    <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/clinic_6.jpg" style="width:100%">
   </div>
 </div>
 </body>
