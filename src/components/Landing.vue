2797d2adc15d75090ea9ddbd6a3f68255ecd22ec
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-13 03:20:48 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -77,6 +77,21 @@ position:absolute;
 }
 </style>
 
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/css/mdb.min.css" rel="stylesheet">
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/js/mdb.min.js"></script>
 <template>
 <body>
 
@@ -108,7 +123,7 @@ position:absolute;
         <p>MAKE AN APPOINTMENT</p>
         <a class="btn-floating blue accent-1"><i class="fas fa-phone"></i></a>
         <p>+977-985-2060177</p>
-        
+
         <table>
           <tr><th>Hours</th><td></td></tr>
           <tr><th>Sunday:</th><td>9am - 5pm</td></tr>
