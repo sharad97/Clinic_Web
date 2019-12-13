8ca70f62c198caed88d818141364f4b0ae820c53
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-13 05:06:50 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -103,19 +103,6 @@ table.center {
     margin-left:-15px;
   }
 
-.map-container{
-overflow:hidden;
-padding-bottom:82%;
-position:relative;
-height:0;
-}
-
-.map-container iframe{
-left:0;
-top:0;
-width:100%;
-position:absolute;
-}
 .call {
 text-align: center;
 }
@@ -154,9 +141,7 @@ text-align: center;
   </div>
 
   <div class="column_2">
-    <div class="map-container">
-      <iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJzSfxBx7H6DkR5PJeX2cmXPA&key=AIzaSyBKtVdM9lqOznPQqEs82kQJanJEga1NhTg" allowfullscreen></iframe>
-    </div>
+      <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJzSfxBx7H6DkR5PJeX2cmXPA&key=AIzaSyBKtVdM9lqOznPQqEs82kQJanJEga1NhTg" allowfullscreen></iframe>
   </div>
 </div>
 
