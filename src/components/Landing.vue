47b48142b15e11bd45e9519bf5872e6615abd95e
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-17 00:29:22 -0800

    adding map

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,60 +1,83 @@
-<script lang="ts">
-import Vue from "vue";
-declare var google: any
-export default Vue.extend({
-  name: 'google-map',
-  props: ['name'],
-  data: function () {
-    let markerArray:any[] = [];
-    return {
-      mapName: this.name + "-map",
-      markerCoordinates: [{
-        latitude: 51.501527,
-        longitude: -0.1921837
-      }, {
-        latitude: 51.505874,
-        longitude: -0.1838486
-      }, {
-        latitude: 51.4998973,
-        longitude: -0.202432
-      }],
-      map: null,
-      bounds: null,
-      markers: markerArray
+<script>
+import gmapsInit from './utils/gmaps';
+const locations = [
+  {
+    position: {
+      lat: 48.160910,
+      lng: 16.383330,
+    },
+  },
+  {
+    position: {
+      lat: 48.174270,
+      lng: 16.329620,
+    },
+  },
+  {
+    position: {
+      lat: 48.146140,
+      lng: 16.297030,
+    },
+  },
+  {
+    position: {
+      lat: 48.135830,
+      lng: 16.194460,
+    },
+  },
+  {
+    position: {
+      lat: 48.306091,
+      lng: 14.286440,
+    },
+  },
+  {
+    position: {
+      lat: 47.503040,
+      lng: 9.747070,
+    },
+  },
+];
+export default {
+  name: `App`,
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+      geocoder.geocode({ address: `Austria` }, (results, status) => {
+        if (status !== `OK` || !results[0]) {
+          throw new Error(status);
+        }
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
+      const markerClickHandler = (marker) => {
+        map.setZoom(13);
+        map.setCenter(marker.getPosition());
+      };
+      const markers = locations
+        .map((location) => {
+          const marker = new google.maps.Marker({ ...location, map });
+          marker.addListener(`click`, () => markerClickHandler(marker));
+          return marker;
+        });
+      
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
     }
   },
-  mounted: function () {
-    this.bounds = new google.maps.LatLngBounds();
-    const element = document.getElementById(this.mapName)
-    const mapCentre = this.markerCoordinates[0]
-    const options = {
-      center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
-    }
-    this.map = new google.maps.Map(element, options);
-    this.markerCoordinates.forEach((coord) => {
-      const position = new google.maps.LatLng(coord.latitude, coord.longitude);
-      const marker = new google.maps.Marker({
-        position,
-        map: this.map
-      });
-      this.markers.push(marker)
-      this.map.fitBounds(this.bounds.extend(position))
-    });
-  }
-});
+};
 </script>
 
-<style scoped>
-  .google-map {
-    width: 800px;
-    height: 600px;
-    margin: 0 auto;
-    background: gray;
-  }
-</style>
 
 <style>
 
+.App {
+  width: 100vw;
+  height: 100vh;
+}
 
 * {
   box-sizing: border-box;
