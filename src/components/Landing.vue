ed909206f5d6d6914e63e7a283eca0d816849bfe
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-17 03:30:06 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,100 +1,111 @@
-new Vue({
-  el: "#map",
-  data: {
-    map: "",
-    popup: "",
-    Popup: "",
+<script>
+export default {
+  data() {
+    return {
+      map: "",
+      popup: "",
+      Popup: ""
+    };
   },
   methods: {
-  	definePopupClass() {
+    definePopupClass() {
       /**
-   * A customized popup on the map.
-   * @param {!google.maps.LatLng} position
-   * @param {!Element} content
-   * @constructor
-   * @extends {google.maps.OverlayView}
-   */
-  this.Popup = function(position, content) {
-    this.position = position;
+       * A customized popup on the map.
+       * @param {!google.maps.LatLng} position
+       * @param {!Element} content
+       * @constructor
+       * @extends {google.maps.OverlayView}
+       */
+      this.Popup = function(position, content) {
+        this.position = position;
 
-    content.classList.add('popup-bubble-content');
+        content.classList.add("popup-bubble-content");
 
-    var pixelOffset = document.createElement('div');
-    pixelOffset.classList.add('popup-bubble-anchor');
-    pixelOffset.appendChild(content);
+        var pixelOffset = document.createElement("div");
+        pixelOffset.classList.add("popup-bubble-anchor");
+        pixelOffset.appendChild(content);
 
-    this.anchor = document.createElement('div');
-    this.anchor.classList.add('popup-tip-anchor');
-    this.anchor.appendChild(pixelOffset);
+        this.anchor = document.createElement("div");
+        this.anchor.classList.add("popup-tip-anchor");
+        this.anchor.appendChild(pixelOffset);
 
-    // Optionally stop clicks, etc., from bubbling up to the map.
-    this.stopEventPropagation();
-  };
-  // NOTE: google.maps.OverlayView is only defined once the Maps API has
-  // loaded. That is why Popup is defined inside initMap().
-  this.Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+        // Optionally stop clicks, etc., from bubbling up to the map.
+        this.stopEventPropagation();
+      };
+      // NOTE: google.maps.OverlayView is only defined once the Maps API has
+      // loaded. That is why Popup is defined inside initMap().
+      this.Popup.prototype = Object.create(google.maps.OverlayView.prototype);
 
-  /** Called when the popup is added to the map. */
-  this.Popup.prototype.onAdd = function() {
-    this.getPanes().floatPane.appendChild(this.anchor);
-  };
+      /** Called when the popup is added to the map. */
+      this.Popup.prototype.onAdd = function() {
+        this.getPanes().floatPane.appendChild(this.anchor);
+      };
 
-  /** Called when the popup is removed from the map. */
-  this.Popup.prototype.onRemove = function() {
-    if (this.anchor.parentElement) {
-      this.anchor.parentElement.removeChild(this.anchor);
-    }
-  };
+      /** Called when the popup is removed from the map. */
+      this.Popup.prototype.onRemove = function() {
+        if (this.anchor.parentElement) {
+          this.anchor.parentElement.removeChild(this.anchor);
+        }
+      };
 
-  /** Called when the popup needs to draw itself. */
-  this.Popup.prototype.draw = function() {
-    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-    // Hide the popup when it is far out of view.
-    var display =
-        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-        'block' :
-        'none';
+      /** Called when the popup needs to draw itself. */
+      this.Popup.prototype.draw = function() {
+        var divPosition = this.getProjection().fromLatLngToDivPixel(
+          this.position
+        );
+        // Hide the popup when it is far out of view.
+        var display =
+          Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
+            ? "block"
+            : "none";
 
-    if (display === 'block') {
-      this.anchor.style.left = divPosition.x + 'px';
-      this.anchor.style.top = divPosition.y + 'px';
-    }
-    if (this.anchor.style.display !== display) {
-      this.anchor.style.display = display;
-    }
-  };
+        if (display === "block") {
+          this.anchor.style.left = divPosition.x + "px";
+          this.anchor.style.top = divPosition.y + "px";
+        }
+        if (this.anchor.style.display !== display) {
+          this.anchor.style.display = display;
+        }
+      };
 
-  /** Stops clicks/drags from bubbling up to the map. */
-  this.Popup.prototype.stopEventPropagation = function() {
-    var anchor = this.anchor;
-    anchor.style.cursor = 'auto';
+      /** Stops clicks/drags from bubbling up to the map. */
+      this.Popup.prototype.stopEventPropagation = function() {
+        var anchor = this.anchor;
+        anchor.style.cursor = "auto";
 
-    ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
-     'pointerdown']
-        .forEach(function(event) {
+        [
+          "click",
+          "dblclick",
+          "contextmenu",
+          "wheel",
+          "mousedown",
+          "touchstart",
+          "pointerdown"
+        ].forEach(function(event) {
           anchor.addEventListener(event, function(e) {
             e.stopPropagation();
           });
         });
-  };
+      };
     }
   },
   mounted() {
-  	this.$nextTick(() => {
+    this.$nextTick(() => {
       this.definePopupClass();
-      this.map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -33.9, lng: 151.1},
-        scrollwheel: false,
-        zoom: 10,
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -33.9, lng: 151.1 },
+        zoom: 10
       });
 
       this.popup = new this.Popup(
-          new google.maps.LatLng(-33.866, 151.196),
-          document.getElementById('content'));
+        new google.maps.LatLng(-33.866, 151.196),
+        document.getElementById("content")
+      );
       this.popup.setMap(this.map);
-    })
+    });
   }
-})
+};
+</script>
 
 
 
@@ -104,6 +115,7 @@ new Vue({
 #map {
   height: 100%;
 }
+
 html, body {
   margin: 0;
   padding: 0;
@@ -154,6 +166,7 @@ html, body {
   max-height: 60px;
   box-shadow: 0px 2px 10px 1px rgba(0,0,0,0.5);
 }
+
 * {
   box-sizing: border-box;
 }
