de08b7583e1eb0f2cb6bfbff19c6c63b9f62a3d0
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-17 03:05:15 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -7,90 +7,22 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }},
-    methods: {
-    function(){
-  var popup, Popup;
-
+  mounted: function() {
         console.log("map: ", google.maps)
         var lat = 27.37834;
         var lng = 87.206579;
+        var uluru = {lat:lat, lng:lng};
         var map = new google.maps.Map(document.getElementById('myMap'), {
-            center: {lat:lat, lng:lng};
+            center: uluru,
             scrollwheel: false,
             zoom: 20
             });
 
-        Popup = createPopupClass();
+        var marker = new google.maps.Marker({position: uluru, map: map});
 
-popup = new Popup(
-    new google.maps.LatLng(lat, lng),
-    document.getElementById('content'));
-popup.setMap(map);
-},
 
-function createPopupClass() {
-
-function Popup(position, content) {
-  this.position = position;
-
-  content.classList.add('popup-bubble');
-
-  // This zero-height div is positioned at the bottom of the bubble.
-  var bubbleAnchor = document.createElement('div');
-  bubbleAnchor.classList.add('popup-bubble-anchor');
-  bubbleAnchor.appendChild(content);
-
-  // This zero-height div is positioned at the bottom of the tip.
-  this.containerDiv = document.createElement('div');
-  this.containerDiv.classList.add('popup-container');
-  this.containerDiv.appendChild(bubbleAnchor);
-
-  // Optionally stop clicks, etc., from bubbling up to the map.
-  google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
-},
-// ES5 magic to extend google.maps.OverlayView.
-Popup.prototype = Object.create(google.maps.OverlayView.prototype);
-
-/** Called when the popup is added to the map. */
-Popup.prototype.onAdd = function() {
-  this.getPanes().floatPane.appendChild(this.containerDiv);
-},
-
-/** Called when the popup is removed from the map. */
-Popup.prototype.onRemove = function() {
-  if (this.containerDiv.parentElement) {
-    this.containerDiv.parentElement.removeChild(this.containerDiv);
   }
-},
 
-/** Called each frame when the popup needs to draw itself. */
-Popup.prototype.draw = function() {
-  var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-
-  // Hide the popup when it is far out of view.
-  var display =
-      Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-      'block' :
-      'none';
-
-  if (display === 'block') {
-    this.containerDiv.style.left = divPosition.x + 'px';
-    this.containerDiv.style.top = divPosition.y + 'px';
-  }
-  if (this.containerDiv.style.display !== display) {
-    this.containerDiv.style.display = display;
-  }
-}
-
-return Popup;
-
-
-}
-  },
-  mounted() {
-   this.function();
-
-}
 }
 </script>
 <style scoped>
@@ -247,9 +179,6 @@ text-align: center;
 
   <div class="column_2">
   <div id="myMap"></div>
-  <div id="content">
-  Hello world!
-</div>
   </div>
 </div>
 
