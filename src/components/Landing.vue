08653353051e02b7c82cd5b461bca4a5ae195c30
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-17 03:07:34 -0800

    map update

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,38 +1,155 @@
+new Vue({
+  el: "#map",
+  data: {
+    map: "",
+    popup: "",
+    Popup: "",
+  },
+  methods: {
+  	definePopupClass() {
+      /**
+   * A customized popup on the map.
+   * @param {!google.maps.LatLng} position
+   * @param {!Element} content
+   * @constructor
+   * @extends {google.maps.OverlayView}
+   */
+  this.Popup = function(position, content) {
+    this.position = position;
 
+    content.classList.add('popup-bubble-content');
 
-<script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }},
-  mounted: function() {
-        console.log("map: ", google.maps)
-        var lat = 27.37834;
-        var lng = 87.206579;
-        var uluru = {lat:lat, lng:lng};
-        var map = new google.maps.Map(document.getElementById('myMap'), {
-            center: uluru,
-            scrollwheel: false,
-            zoom: 20
-            });
+    var pixelOffset = document.createElement('div');
+    pixelOffset.classList.add('popup-bubble-anchor');
+    pixelOffset.appendChild(content);
 
-        var marker = new google.maps.Marker({position: uluru, map: map});
+    this.anchor = document.createElement('div');
+    this.anchor.classList.add('popup-tip-anchor');
+    this.anchor.appendChild(pixelOffset);
+
+    // Optionally stop clicks, etc., from bubbling up to the map.
+    this.stopEventPropagation();
+  };
+  // NOTE: google.maps.OverlayView is only defined once the Maps API has
+  // loaded. That is why Popup is defined inside initMap().
+  this.Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+  /** Called when the popup is added to the map. */
+  this.Popup.prototype.onAdd = function() {
+    this.getPanes().floatPane.appendChild(this.anchor);
+  };
+
+  /** Called when the popup is removed from the map. */
+  this.Popup.prototype.onRemove = function() {
+    if (this.anchor.parentElement) {
+      this.anchor.parentElement.removeChild(this.anchor);
+    }
+  };
+
+  /** Called when the popup needs to draw itself. */
+  this.Popup.prototype.draw = function() {
+    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+    // Hide the popup when it is far out of view.
+    var display =
+        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+        'block' :
+        'none';
+
+    if (display === 'block') {
+      this.anchor.style.left = divPosition.x + 'px';
+      this.anchor.style.top = divPosition.y + 'px';
+    }
+    if (this.anchor.style.display !== display) {
+      this.anchor.style.display = display;
+    }
+  };
+
+  /** Stops clicks/drags from bubbling up to the map. */
+  this.Popup.prototype.stopEventPropagation = function() {
+    var anchor = this.anchor;
+    anchor.style.cursor = 'auto';
+
+    ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
+     'pointerdown']
+        .forEach(function(event) {
+          anchor.addEventListener(event, function(e) {
+            e.stopPropagation();
+          });
+        });
+  };
+    }
+  },
+  mounted() {
+  	this.$nextTick(() => {
+      this.definePopupClass();
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.9, lng: 151.1},
+        zoom: 10,
+      });
+
+      this.popup = new this.Popup(
+          new google.maps.LatLng(-33.866, 151.196),
+          document.getElementById('content'));
+      this.popup.setMap(this.map);
+    })
 
 
   }
-
-}
-</script>
-<style scoped>
-    #myMap {
-    height:300px;
-    width: 100%;
-   }
-</style>
+})
 
 <style>
+
+/* Always set the map height explicitly to define the size of the div
+ * element that contains the map. */
+#map {
+  height: 100%;
+}
+
+/* The location pointed to by the popup tip. */
+.popup-tip-anchor {
+  height: 0;
+  position: absolute;
+  /* The max width of the info window. */
+  width: 200px;
+}
+/* The bubble is anchored above the tip. */
+.popup-bubble-anchor {
+  position: absolute;
+  width: 100%;
+  bottom: /* TIP_HEIGHT= */ 8px;
+  left: 0;
+}
+/* Draw the tip. */
+.popup-bubble-anchor::after {
+  content: "";
+  position: absolute;
+  top: -1px;
+  left: 0;
+  /* Center the tip horizontally. */
+  transform: translate(-50%, 0);
+  /* The tip is a https://css-tricks.com/snippets/css/css-triangle/ */
+  width: 0;
+  height: 0;
+  /* The tip is 8px high, and 12px wide. */
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-top: /* TIP_HEIGHT= */ 8px solid white;
+}
+/* The popup bubble itself. */
+.popup-bubble-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(-50%, -100%);
+  /* Style the info window. */
+  background-color: white;
+  padding: 5px;
+  border-radius: 3px;
+  font-family: sans-serif;
+  overflow-y: auto;
+  max-height: 60px;
+  box-shadow: 0px 2px 10px 1px rgba(0,0,0,0.5);
+}
 
 * {
   box-sizing: border-box;
@@ -178,8 +295,10 @@ text-align: center;
   </div>
 
   <div class="column_2">
-  <div id="myMap"></div>
-  </div>
+  <div id="map"></div>
+  <div id="content">
+    Hello this is me
+  </div>  </div>
 </div>
 
 
