1de6ebdd6cddaf673ef1652c73ef1268938b3142
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-18 04:44:56 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -296,9 +296,9 @@ padding-top: 10px;
 display: block;
 margin-left: auto;
 margin-right: auto;
-height:auto; 
+height:auto;
 max-height: 500px;
-max-width: 100%
+max-width: 100%;
 }
 
 </style>
@@ -325,11 +325,11 @@ max-width: 100%
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/slide_1.jpg" alt="slide_1" class"slides">
+        <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/slide_1.jpg" alt="slide_1" class="slides">
       </div>
 
       <div class="item">
-        <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/slide_2.jpg" alt="slide_2" class"slides">
+        <img src="https://12webimage.s3-us-west-1.amazonaws.com/clinic_web/slide_2.jpg" alt="slide_2" class="slides">
       </div>
     </div>
 
