b64827597570f78300cd60fe101b0d719f15c323
Sharad Shrestha <sshrest3@ucsc.edu>
2019-12-18 00:46:05 -0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -298,15 +298,15 @@ text-align: center;
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+        <img src="https://www.quanzhanketang.com/bootstrap/la.jpg" alt="Los Angeles" style="width:100%;">
       </div>
 
       <div class="item">
-        <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+        <img src="https://www.quanzhanketang.com/bootstrap/chicago.jpg" alt="Chicago" style="width:100%;">
       </div>
 
       <div class="item">
-        <img src="ny.jpg" alt="New york" style="width:100%;">
+        <img src="https://www.quanzhanketang.com/bootstrap/ny.jpg" alt="New york" style="width:100%;">
       </div>
     </div>
 
